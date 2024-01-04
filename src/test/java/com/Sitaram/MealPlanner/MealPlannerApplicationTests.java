572165e4d2da897270818c5040be04851250f8f4
sita rama kumar <90118.kanagala@students.itu.edu>
2024-01-04 17:49:05 +0530

    MealPlanner Project

---
+++ src/test/java/com/Sitaram/MealPlanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sitaram.MealPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
